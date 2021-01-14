20ed6fa9b019644c28f0936faacbb5cbf92c7ab6
Ivan Velazquez <Ivanmichel@MacBook-Pro-de-Ivan-6.local>
2021-01-14 11:37:04 -0600

    Analysis made for each passage

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import re
 
-file ="raw_data/paragraph_1.txt"
+file ="raw_data/paragraph_2.txt"
+text_analysis = "analysis/output_analysis_1.txt"
+
 
 passage = ''
 
@@ -22,8 +24,36 @@ for word in word_split:
     letter_count.append(len(word))
 
 # Calculate the avg letter count
-avg_letter_count = sum(letter_count) / float(len(letter_count))
+avg_letter_count = round(sum(letter_count) / float(len(letter_count)),2)
 
 sentences = re.split('[.|?|!]', passage)
 print(sentences)
 
+no_sentences = len(sentences)
+
+words_in_sentence = []
+
+for sentence in sentences:
+    words_in_sentence.append(len(sentence.split(" ")))    
+
+#Average words per sentence
+
+avg_sentence = sum(words_in_sentence)/float(len(words_in_sentence))
+
+#Paragraph Analysis
+
+text = (
+    f"\nParagraph Analysis\n"
+    f"-----------------\n"
+    f"Approximate Word Count: {word_count}\n"
+    f"Approximate Sentence Count: {no_sentences}\n"
+    f"Average Letter Count: {avg_letter_count}\n"
+    f"Average Sentence Length: {avg_sentence}\n"
+)
+
+# Print to terminal
+print(text)
+
+# Save the results to analysis text file
+with open(text_analysis, "a") as txt_file:
+    txt_file.write(text)
